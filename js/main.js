db200916870fae95be39d7db68fefeeec9f29973
Kareem ashraf <ashrafkareem005@gmail.com>
2024-10-29 10:46:11 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ const testmonal=[
         paragrap:" Ive been interested in coding for a while but never taken the jump, until now. I couldnt recommend this course enough Im now in the job of my dreams and so excited about the future.",
         nam:"Tanya Sinclair",
         rol:" UX Engineer",
-        img:"../images/image-tanya.jpg"
+        img:"images/image-tanya.jpg"
     },
     {
         paragrap:" “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
         nam:"John Tarkpor",
         rol:" Junior Front-end Developer",
-        img:"../images/image-john.jpg"   
+        img:"images/image-john.jpg"   
     }
 ];
 let currentindex=0;
